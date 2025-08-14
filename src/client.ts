3abf3205e5eb7163db852cae0b98d36552f2bd7a
Niels Krijnen <niels.krijnen@icloud.com>
2025-08-14 21:45:32 +0200

    - Add `getTicket` and `getTicketDetailed` methods to fetch and process ticket information. - Extend `getRelation` and `getRelationDetailed` methods with additional fields and improved data handling. - Remove unused `getRelationId` method and refactor `getInputValue`.

--- src/client.ts
+++ src/client.ts
@@ -72,6 +72,84 @@ export class WTicketScraper {
     })
   }
 
+  async getTicketDetailed(id: number) {
+    await this.page.goto(`https://wticket-pcrolin.multitrader.nl/jsp/wf/uiform/uiform_wf1act.jsp?uniqueid=${id}`)
+
+    const relation = await this.page.$eval("#groupRelation", div => {
+      const tds = div.querySelectorAll("td")
+      const link = tds[1].querySelector("a")!
+      const onclick = link.getAttribute("onclick")!
+      const id = Number(onclick.split("(")[1].split(")")[0])
+      const strong = link.querySelectorAll("strong")
+
+      return {
+        id,
+        relationNumber: Number(strong[0].textContent),
+        name: strong[1].textContent,
+        location: tds[3].textContent.trim()
+      }
+    })
+
+    const involved = await this.page.$$eval(".users", tables => {
+      return {
+        inCharge: tables[0].querySelector("td")!.textContent,
+        practitioners: tables[1].querySelector("td")!.textContent
+      }
+    })
+
+    const comments = await this.page.$eval("#groupComments", div => {
+      const comments = Array.from(div.querySelectorAll(".comment"))
+
+      return comments.map(comment => {
+        const id = Number(comment.getAttribute("id")!.replace("comment", ''))
+        const desc = comment.querySelector(".desc")!
+        const timestamp = comment.querySelector(".timestamp")!
+        const author = comment.querySelector(".author")!
+        const internal = !!comment.querySelector(".internal")
+        const message = comment.querySelector(".atsc-mime-message")
+
+        return {
+          id,
+          description: desc.textContent,
+          timestamp: timestamp.textContent,
+          author: author.textContent,
+          internal,
+          message: message?.innerHTML ?? null
+        }
+      })
+    })
+
+    return {
+      id,
+      relation,
+      involved,
+      comments: comments.map(comment => {
+        const [datePart, timePart] = comment.timestamp.split(" ");
+        const [day, month, year] = datePart.split("-").map(Number);
+        const [hour, minute] = timePart.split(":").map(Number);
+        return {
+          ...comment,
+          timestamp: new Date(year, month - 1, day, hour, minute)
+        }
+      })
+    }
+  }
+
+  async getTicket(ticketNumber: number) {
+    await this.page.goto(`https://wticket-pcrolin.multitrader.nl/jsp/atsc/UITableIFrame.jsp?queryid=wf1act&maxrows=1&searchcol=2&key=_%3Cexact%3E_${ticketNumber}`)
+
+    const row = (await this.page.$$("tr"))[2]
+    return row.evaluate(tr => {
+      const tds = tr.querySelectorAll("td")
+      return {
+        id: Number(tr.getAttribute("unid")),
+        number: Number(tds[1].textContent),
+        searchName: tds[2].textContent,
+        description: tds[3].textContent
+      }
+    })
+  }
+
   async listTicketsRaw() {
     await this.page.goto("https://wticket-pcrolin.multitrader.nl/jsp/atsc/UITableIFrame.jsp?queryid=wf1act")
 
@@ -88,17 +166,40 @@ export class WTicketScraper {
   }
 
   async getRelation(relationNumber: number) {
-    const id = await this.getRelationId(relationNumber)
+    await this.page.goto(`https://wticket-pcrolin.multitrader.nl/jsp/atsc/UITableIFrame.jsp?queryid=2&searchcol=2&key=_%3Cexact%3E_${relationNumber}&maxrows=1`)
+
+    const row = (await this.page.$$("tr"))[2]
+    const data = JSON.parse(await row.evaluate(tr => tr.getAttribute("data-hidden-columns") as string))
+    const id = Number(data.gc1rel_unid)
+    return row.$$eval("td", (tds, id) => {
+      return {
+        id,
+        searchName: tds[0].textContent,
+        relationNumber: Number(tds[1].textContent),
+        shortName: tds[2].textContent,
+        phone1: tds[3].textContent === "" ? null : tds[3].textContent,
+        phone2: tds[10].textContent === "" ? null : tds[10].textContent,
+        mobilePhone: tds[4].textContent === "" ? null : tds[4].textContent,
+        name: tds[5].textContent,
+        address: tds[6].textContent === "" ? null : tds[6].textContent,
+        zipCode: tds[7].textContent,
+        city: tds[8].textContent === "" ? null : tds[8].textContent,
+        countryCode: tds[9].textContent,
+      }
+    }, id)
+  }
+
+  async getRelationDetailed(id: number) {
     await this.page.goto(`https://wticket-pcrolin.multitrader.nl/jsp/gc/uiform_gc1rel.jsp?uniqueid=${id}`)
 
     return {
       id,
       naw: {
         number: Number(await this.getInputValue("#relnr")),
-        name: await this.getInputValue("#naam1"),
+        name: await this.getInputValue("#naam1") as string,
         nameAddition: await this.getInputValue("#naam2"),
-        searchName: await this.getInputValue("#zoeknaam"),
-        shortName: await this.getInputValue("#naamkort"),
+        searchName: await this.getInputValue("#zoeknaam") as string,
+        shortName: await this.getInputValue("#naamkort") as string,
         invoiceName: await this.getInputValue("#tavnaam"),
         establishment: {
           street: await this.getInputValue("#straat"),
@@ -123,43 +224,25 @@ export class WTicketScraper {
           province: await this.getInputValue("#p_provincie"),
           countryCode: await this.getInputValue("#p_landcod"),
           country: await this.getInputValue("#p_land")
+        },
+        other: {
+          phone1: await this.getInputValue("#telnr1"),
+          phone2: await this.getInputValue("#telnr2"),
+          mobilePhone: await this.getInputValue("#mobnr"),
+          fax: await this.getInputValue("#faxnr"),
+          email: await this.getInputValue("#e_adres"),
+          groups: await this.getInputValue("#gbrgrpcod"),
+          website: await this.getInputValue("#website"),
+          webshop: await this.getInputValue("#webshop"),
+          facebook: await this.getInputValue("#facebook"),
+          instagram: await this.getInputValue("#instagram"),
         }
       }
     }
   }
 
   getInputValue(selector: string) {
-    return this.page.$eval(selector, input => input.getAttribute("value") as string)
-  }
-
-  async getRelationId(relationNumber: number) {
-    await this.page.goto("https://wticket-pcrolin.multitrader.nl/jsp/wf/index.jsp")
-
-    const frame = await this.page.$("#wmain").then(el => el?.contentFrame() ?? null)
-    if (frame) {
-      await frame.type("#searchfield", String(relationNumber))
-      await this.page.waitForNetworkIdle()
-
-      const entityItem = await frame.$(".entityItem")
-      if (entityItem) {
-        const link = await entityItem.$(".link")
-        if (link) {
-          const dataAction = await link.evaluate(div => div.getAttribute("data-action"))
-          if (dataAction) {
-            const split = dataAction.split('=')
-            return Number(split[split.length - 1])
-          } else {
-            throw new Error("Couldn't find data-action")
-          }
-        } else {
-          throw new Error("Couldn't find link")
-        }
-      } else {
-        throw new Error("Couldn't find entityItem")
-      }
-    } else {
-      throw new Error("Couldn't find main frame")
-    }
+    return this.page.$eval(selector, input => input.getAttribute("value"))
   }
 
   async close() {
